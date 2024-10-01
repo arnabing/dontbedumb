dbb65a55896e05703cf3bde9529e035b14232f36
Arnab <arnabing@Arnabs-MacBook-Pro.local>
2024-10-01 14:00:27 -0400

    Fix frontend build: Add style-loader and update webpack config

--- ui/next.config.js
+++ ui/next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
+      use: ['css-loader', 'postcss-loader'],
     });
     return config;
   },
